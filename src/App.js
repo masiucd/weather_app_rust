16610207cfd58347dfaccb36d9ead08525d455d7
Marcell CISZEK <marcell.ciszek@alten.se>
2019-06-05 13:10:04 +0200

    'navigation'

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './sass/main.scss';
 import Header from './components/Header';
+import Navigation from './components/Navigation';
 import About from './components/About';
 import Feauture from './components/Feauture';
 import Tours from './components/Tours';
@@ -13,6 +14,7 @@ export default function App() {
   return (
     <>
       <BrowserRouter>
+        <Navigation />
         <Header />
         <About />
         <Feauture />
