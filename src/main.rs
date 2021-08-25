fa74a9d717e0582dc37f13774cf269e18fff0244
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-08-25 08:49:10 +0200

    with ENV

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    // 530b2c542c88740968964b9c17fbcb7ae31db244
-    let t = std::env::var("KEY");
-    dbg!("token {}", t);
+    let t = std::env::var("WEATHER_API_KEY").expect("Oppps");
+    dbg!("token {}", &t);
+    println! {"token: {}", t};
 }
