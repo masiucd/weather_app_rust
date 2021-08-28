0f476e58e16e8d05ff46d845a4e366dc5fc3cd87
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-08-28 11:24:34 +0200

    get city

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 fn main() {
-    let t = std::env::var("WEATHER_API_KEY").expect("Oppps");
-    dbg!("token {}", &t);
-    println! {"token: {}", t};
+    let api_token = std::env::var("WEATHER_API_KEY").expect("Oppps");
+    let mut arg_iter = std::env::args();
+    arg_iter.next();
+    let args: String = arg_iter.collect();
+
+    let client = reqwest::blocking::Client::new();
+    let response = client
+        .get("https://api.waqi.info/search/")
+        .query(&[("token", api_token), ("keyword", args)])
+        .header(reqwest::header::USER_AGENT, "masius weather app")
+        .send()
+        .expect("Successfully JSON request")
+        .json::<serde_json::Value>()
+        .expect("expected the body to be json");
+
+    // println!("{}", response)
+    dbg!(response);
 }
